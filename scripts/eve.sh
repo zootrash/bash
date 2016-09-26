35ac2cce7478a073338d39f46c78113881e09633
zootrash <zootrash@users.noreply.github.com>
2016-09-26 14:54:01 -0700

    working on eve

--- scripts/eve.sh
+++ scripts/eve.sh
@@ -3,7 +3,24 @@
 # personal ai
 # command: eve
 
-echo "Yes?"
-read response
+# def text colors
+bldpur='\e[1;35m' # bold purple
+txtwht='\e[0;37m' # text white
 
+# ai and user text color
+ai_color=$bldpur
+u_color=$txtwht
+
+# first response
+echo -e "${ai_color}Yes?"
+echo -e "${u_color}"
+read RESPONSE
+
+# user wants to know the time
+REQ_TIME="time"
+if [ $RESPONSE == $REQ_TIME ]; then
+	echo "i found the word time"
+else
+	echo "nope didnt work"
+fi
 
