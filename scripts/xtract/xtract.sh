59c71ebea9fafdfb1a72f5a5b7f2b9e257807210
zootrash <zootrash@users.noreply.github.com>
2016-10-11 14:11:49 -0700

    upd xtract for location specified

--- scripts/xtract/xtract.sh
+++ scripts/xtract/xtract.sh
@@ -10,28 +10,43 @@
 
 # define variables
 
-# number of arguments check
-if [ $# -ne 1 ]; then
-	echo "Error: Must include one argument."
-	exit
-fi
+# check if file exists and select appr. extraction program
+if [[ $# -eq 1 ]]; then
+	FILEDIR=$(dirname $1)
+	echo "xtract: Extracting to directory $FILEDIR"
 
-# check if file exists
-if [ -f $1 ]; then
-	# select appr. extraction program
-	case $1 in
-		
-		# .tar.gz extraction
-		*.tar.gz)					tar -xvzf $1 
-		echo "tar.gz xtract success!"
-		;;
+	if [ -f $1 ]; then
+		case $1 in
+			*.tar.gz)					tar -xvzf $1 -C $FILEDIR			# .tar.gz extraction
+			echo "xtract: tar.gz extraction success!" ;;
 
-		# invalid format
-		*)								echo "xtract failed: $1 is an invalid format." ;;
+			*)								echo "xtract: $1 is an invalid format. Extraction failed." ;;
 
-	esac
-	# select appr. extraction program
+		esac
+	else
+		echo "xtract: File $1 does not exist. Extraction failed."
+		exit
+	fi
+
+# check if both arguments exist
+elif [[ $# -eq 2 ]]; then
+	echo "xtract: Extracting to directory: $2"
+
+	if [ -f $1 -a -d $2 ]; then
+		case $1 in
+			*.tar.gz)					tar -xvzf $1 -C $2						# .tar.gz extraction
+			echo "xtract: tar.gz extraction success!" ;;
+
+			*)								echo "xtract: $1 is an invalid format. Extraction failed." ;;
+
+		esac
+	else
+		echo "xtract: Invalid file $1 or directory $2. Extraction failed."
+		exit
+	fi
 
 else
-	echo "File $1 does not exist."
-fi
+	echo "xtract: Invalid number of arguments."
+	exit
+
+fi 
