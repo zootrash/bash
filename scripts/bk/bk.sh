06a4e258a29d08cf619e240b5bd912b3bdbc7fee
zootrash <zootrash@users.noreply.github.com>
2017-01-19 00:16:51 -0800

    working on bk 2.0

--- scripts/bk/bk.sh
+++ scripts/bk/bk.sh
@@ -4,23 +4,53 @@
 #
 ####################################
 #
-# bk 1.0
+# bk 1.0 
 #
 ####################################
 
-### main ###
-echo "Enter the file name or directory you wish to backup: "
-read FILE
-
-if [ -f $FILE ]; then
-	echo "File $FILE found. Backing up now..."
-	cp $FILE $FILE.backup
-	echo "Backup completed."
-elif [ -d $FILE ]; then
-	echo "Directory $FILE found. Backing up now..."
-	cp -r $FILE $FILE.backup
-	echo "Backup completed."
+# bk 2.0 (Working)
+if [ $# -eq 0 ]; then
+	echo "bk.Error: Must include arguments."
+	echo
 else
-	echo "File $FILE does not exist."
+	for FILE in "$@"
+	do
+		if [ -f $FILE ]; then
+			echo "File $FILE found. Backing up now . . ."
+			cp $FILE $FILE.backup
+		elif [ -d $FILE ]; then
+			echo "Directory $FILE found. Backing up now . . ."
+
+			# check if last character is a slash
+			if [[ $FILE = */ ]]; then
+				echo "hello"
+				#cp -r test/ test.backup/
+
+			else
+				#cp -r test test.backup/
+				cp -r $FILE $FILE.backup
+			
+
+			fi
+		else
+			echo "File $FILE does not exist."
+		fi
+	done
 fi
+
+### main ###
+#echo "Enter the file name or directory you wish to backup: "
+#read FILE
+#
+#if [ -f $FILE ]; then
+#	echo "File $FILE found. Backing up now..."
+#	cp $FILE $FILE.backup
+#	echo "Backup completed."
+#elif [ -d $FILE ]; then
+#	echo "Directory $FILE found. Backing up now..."
+#	cp -r $FILE $FILE.backup
+#	echo "Backup completed."
+#else
+#	echo "File $FILE does not exist."
+#fi
 ### end main ###
